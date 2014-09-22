3f8ce70c65180687b99d3a0e5d22b388d7520e05
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2014-09-23 02:31:29 +0300

    Update AFP configuration

--- chef/cookbooks/afp/recipes/default.rb
+++ chef/cookbooks/afp/recipes/default.rb
@@ -1,7 +1,26 @@
-package "netatalk"
+# Users should enable Time Machine on remote disks
+# defaults write com.apple.systempreferences TMShowUnsupportedNetworkVolumes 1
 
-cookbook_file "afp" do
-  path "/etc/rc.conf.d/afp"
+package "netatalk"
+package "nss_mdns"
+
+cookbook_file "dbus" do
+  path "/etc/rc.conf.d/dbus"
+  action :create
+end
+
+cookbook_file "netatalk" do
+  path "/etc/rc.conf.d/netatalk"
+  action :create
+end
+
+cookbook_file "avahi_daemon" do
+  path "/etc/rc.conf.d/avahi_daemon"
+  action :create
+end
+
+cookbook_file "avahi_dnsconfd" do
+  path "/etc/rc.conf.d/avahi_dnsconfd"
   action :create
 end
 
@@ -15,11 +34,16 @@ cookbook_file "AppleVolumesTimeMachine.default" do
   action :create
 end
 
+cookbook_file "afpd.service" do
+  path "/usr/local/etc/avahi/services/afpd.service"
+  action :create
+end
+
 user 'imkoleva' do
     comment "Ivelina Koleva"
     home "/home/imkoleva"
     shell "/bin/nologin"
-    password "$6$i5g8qYMsCjPhBrAX$UYaNi9UonZ42mumso2kmM3EglOgoMCJLZAkO8CFkGAxgjZ.Yx7fnF.uTKGcB19RP3JRfzeFOydZaq1joWBWMh."
+    password "$6$RwwunGQsfZDLaWeG$FkKTvZ7Zhp/hLiy1CegzmWEEl99AiK1jhw.BaLCcoleoqReewadnNy1.k.WZFDMT5WPFWD7HcELCe1OdtcssC/"
 end
 
 directory "/storage-a/timecapsule/imkoleva" do
@@ -34,7 +58,9 @@ service "dbus" do
 end
 
 service "netatalk" do
-  action [:start, :reload]
+  supports :status => false
+  pattern "afpd"
+  action [:start]
 end
 
 service "avahi-daemon" do
