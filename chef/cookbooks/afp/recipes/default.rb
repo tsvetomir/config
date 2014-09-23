65b23dc4b8681a3f7f64a4e046952888949884a2
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2014-09-23 23:24:10 +0300

    Move shares into a separate recipe

--- chef/cookbooks/afp/recipes/default.rb
+++ chef/cookbooks/afp/recipes/default.rb
@@ -39,20 +39,6 @@ cookbook_file "afpd.service" do
   action :create
 end
 
-user 'imkoleva' do
-    comment "Ivelina Koleva"
-    home "/home/imkoleva"
-    shell "/bin/nologin"
-    password "$6$RwwunGQsfZDLaWeG$FkKTvZ7Zhp/hLiy1CegzmWEEl99AiK1jhw.BaLCcoleoqReewadnNy1.k.WZFDMT5WPFWD7HcELCe1OdtcssC/"
-end
-
-directory "/storage-a/timecapsule/imkoleva" do
-    owner "imkoleva"
-    group "imkoleva"
-    mode "0770"
-    action :create
-end
-
 service "dbus" do
   action [:start]
 end
