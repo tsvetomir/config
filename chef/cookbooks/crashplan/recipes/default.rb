df9f4d52041965507277e8c59ba38fffa0c078bb
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2014-09-26 01:30:51 +0300

    Add more Crashplan configuration

--- chef/cookbooks/crashplan/recipes/default.rb
+++ chef/cookbooks/crashplan/recipes/default.rb
@@ -1,5 +1,5 @@
-remote_file "/usr/ports/distfiles/jre-7u67-linux-i586.tar.gz" do
-    source "file:///storage-a/install/jre-7u67-linux-i586.tar.gz"
+remote_file '/usr/ports/distfiles/jre-7u67-linux-i586.tar.gz' do
+    source 'file:///storage-a/install/jre-7u67-linux-i586.tar.gz'
     action :create
 end
 
@@ -9,3 +9,11 @@ package 'linux-crashplan' do
     source '/ports/sysutils/linux-crashplan'
 end
 
+cookbook_file 'crashplan' do
+  path '/etc/rc.conf.d/crashplan'
+  action :create
+end
+
+service 'crashplan' do
+  action :start
+end
