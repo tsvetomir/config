8adddec49b1681e3a650f82d54708bd227cfe09e
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2014-10-06 02:06:05 +0300

    Disable PLEX logging

--- chef/cookbooks/plex/recipes/default.rb
+++ chef/cookbooks/plex/recipes/default.rb
@@ -1,11 +1,22 @@
 package 'plexmediaserver'
 
-cookbook_file "plexmediaserver" do
-  path "/etc/rc.conf.d/plexmediaserver"
+cookbook_file 'plexmediaserver' do
+  path '/etc/rc.conf.d/plexmediaserver'
   action :create
 end
 
-service "plexmediaserver" do
+directory '/usr/local/plexdata/Plex Media Server/Logs' do
+    owner 'root'
+    group 'wheel'
+end
+
+link '/usr/local/plexdata/Plex Media Server/Logs/Plex Media Server.log' do
+    owner 'root'
+    group 'wheel'
+    to '/dev/null'
+end
+
+service 'plexmediaserver' do
     action :start
     supports :status => true
 end
