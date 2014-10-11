84c183039f5e3c812e926b66f0103e263bd6c78d
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2014-10-11 22:59:20 +0300

    Move FreeBSD NAS recipes in separate repo

--- chef/solo.rb
+++
@@ -1,7 +0,0 @@
-require 'tmpdir'
-root = File.absolute_path(File.dirname(__FILE__))
-file_cache_path File.join(Dir.tmpdir, 'chef')
-FileUtils.mkdir_p file_cache_path
-
-cookbook_path File.join(root, 'cookbooks')
-ssl_verify_mode :verify_peer
